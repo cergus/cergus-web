81c6b7ebed8ac0846ac71086ae0510f21300b15e
unknown <osman@gmail.com>
2026-04-18 22:36:50 +0300

    fix nextauth route type

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,12 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 
-const handler = NextAuth({
+export const { handlers: { GET, POST } } = NextAuth({
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID!,
       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
     }),
   ],
+  secret: process.env.AUTH_SECRET,
 })
-
-export { handler as GET, handler as POST }
