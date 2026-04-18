ec4ed13ae14b4a63cf9f88035242ca18cc165cc7
unknown <osman@gmail.com>
2026-04-18 20:57:25 +0300

    fix auth route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,13 @@
-import { handlers } from "@/auth"
+import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
 
-export const { GET, POST } = handlers
+const handler = NextAuth({
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+  ],
+})
+
+export { handler as GET, handler as POST }
